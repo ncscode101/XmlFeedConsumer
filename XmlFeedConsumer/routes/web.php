769ec006d17736cec0941f69b0e1ec5ff064d306
Neil Salazar <salazar.neil@gmail.com>
2024-07-17 12:55:06 +0400

    Delete XmlFeedConsumer directory

--- XmlFeedConsumer/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use ncscode101\XmlFeedConsumer\Http\Controllers\XmlFeedController;
-
-Route::get('xml-feed', [XmlFeedController::class, 'index']);
--- XmlFeedConsumer/src/config/xmlfeed.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'url' => env('XML_FEED_URL', 'default-feed-url-here'),
-];
